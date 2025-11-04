0af1fc4844d17d2c082b3ef5bc65c0b3de7b7ec9
jeluard <jeluard@users.noreply.github.com>
2025-11-04 19:48:55 +0100

    fix: added timeout to rsync
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- scripts/helper/sync-overlays.sh
+++ scripts/helper/sync-overlays.sh
@@ -14,8 +14,8 @@ SSH_OPTS="${SSH_OPTS:-}"
 [[ -d "${OVERLAYS_DIR}" ]] || { echo "Error: '${OVERLAYS_DIR}' directory not found."; exit 1; }
 
 echo "🔄 Uploading overlays/ → ${SSH_REMOTE}:/ ..."
-rsync -rptl --progress --partial -e "ssh ${SSH_OPTS}" -r "${OVERLAYS_DIR}/home/pi/" "$SSH_REMOTE:./"
-rsync -rlt --progress --partial -e "ssh ${SSH_OPTS}" --rsync-path="sudo rsync" "${OVERLAYS_DIR}/" "$SSH_REMOTE:/" --exclude="home/"
+rsync -rptl --progress --partial -e "ssh -o ConnectTimeout=5 -o BatchMode=yes -o ConnectionAttempts=1 ${SSH_OPTS}" -r "${OVERLAYS_DIR}/home/pi/" "$SSH_REMOTE:./"
+rsync -rlt --progress --partial -e "ssh -o ConnectTimeout=5 -o BatchMode=yes -o ConnectionAttempts=1 ${SSH_OPTS}" --rsync-path="sudo rsync" "${OVERLAYS_DIR}/" "$SSH_REMOTE:/" --exclude="home/"
 echo "✅ Upload complete."
 
 echo "🔨 Configuring scripts on Pi..."
