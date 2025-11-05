fbe5ae693addcc7a29299c0aacf82b96d28d8147
jeluard <jeluard@users.noreply.github.com>
2025-11-05 23:56:43 +0100

    fix: wrong location
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- overlays/home/pi/scripts/setup/amaru.sh
+++ overlays/home/pi/scripts/setup/amaru.sh
@@ -4,19 +4,20 @@ set -euo pipefail
 
 echo 'export PATH="$PATH:/home/pi/bin"' >> ~/.profile
 
-SETUP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SCRIPT_DIR="$(realpath "$SETUP_DIR/..")"
-ARCHIVE="$(realpath "$SCRIPT_DIR/../bin/dbs.tar.gz")"
-if [ ! -f "$ARCHIVE" ]; then
-  echo "⚠️ Warning: Archive '$ARCHIVE' does not exist."
+BIN_DIR="/home/pi/bin"
+ARCHIVE="${BIN_DIR}/dbs.tar.gz"
+if [ ! -f "${ARCHIVE}" ]; then
+  echo "⚠️ Warning: Archive '${ARCHIVE}' does not exist."
   exit 0
 fi
+
+cd ${BIN_DIR}
 
 set -- *.db # Sets all `*.db` file as positional parameters
 if [ -e "$1" ] && [ -z "${FORCE_UNPACK:-}" ]; then
-  echo "🔄 Found existing .db files. Skipping unpack. (Use FORCE_UNPACK=1 to override.)"
+  echo "➡️ Found existing .db files. Skipping unpack. (Use FORCE_UNPACK=1 to override.)"
   exit 0
 fi
 
-echo "➡️ Extracting $ARCHIVE..."
-tar -xf "$ARCHIVE"
+echo "➡️ Extracting ${ARCHIVE}..."
+tar -xf "${ARCHIVE}"
