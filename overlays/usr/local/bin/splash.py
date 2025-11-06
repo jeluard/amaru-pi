47dfd0252cab236baca080ebf7a779273e0bd0e0
jeluard <jeluard@users.noreply.github.com>
2025-11-06 09:36:36 +0100

    fix: double display init
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- overlays/usr/local/bin/splash.py
+++ overlays/usr/local/bin/splash.py
@@ -7,10 +7,11 @@ from displayhatmini import DisplayHATMini
 width = DisplayHATMini.WIDTH
 height = DisplayHATMini.HEIGHT
 buffer = Image.new("RGB", (width, height))
+hat = DisplayHATMini(buffer)
+hat.display()
 image = Image.open("/home/pi/logo.png").convert("RGB")
 image = image.resize((width, height))
 buffer.paste(image, (0, 0))
-hat = DisplayHATMini(buffer)
 hat.set_led(0, 0, 0)
 
 hat.display()
